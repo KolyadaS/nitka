ec0f2caf4a84a7b73d45f5654e37954c5119901c
Svetlana Kolyada <pfotka@yandex.ru>
2026-05-28 11:00:18 +0400

    fix: update sun skirt drafting calculations 2

--- nitka/src/core/geometry/SunSkirt.ts
+++ nitka/src/core/geometry/SunSkirt.ts
@@ -11,28 +11,29 @@ export interface PatternPart {
 }
 
 export function generateSkirtPattern(m: Measurements): PatternPart[] {
+  const pi = 3.14;
   const seamAllowance = 10;
-  const waistAllowance = 15;
+  const waistAllowance = 0.0214; // 1.5 см на талию 70 см, 3 см на талию 140 см
   const hemAllowance = 15;
-  const beltClosureAllowance = 50;
   const GAP = 10;
 
   const beltWidth = m.beltWidth;
-  const waistRadius = Math.round(m.waist / 6.28) + waistAllowance;
+  const beltClosureAllowance = beltWidth * 1.5;
+  const waistRadius = Math.round((m.waist * (1 + waistAllowance)) / 2 / pi);
   const lengthRadius = waistRadius + m.length;
-  const hipsRadius = Math.round(m.hips / 6.28);
+  const hipsRadius = Math.round(m.hips / 2 / pi);
 
   const beltBase: PathCommand[] = [
     { type: "M", x: 0 + seamAllowance, y: 0 + seamAllowance }, // A
     {
       type: "L",
       // x: m.waist + beltClosureAllowance + seamAllowance,
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance,
       y: 0 + seamAllowance,
     }, // B
     {
       type: "L",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance,
       y: beltWidth * 2 + seamAllowance,
     }, // C
     { type: "L", x: 0 + seamAllowance, y: beltWidth * 2 + seamAllowance }, // D
@@ -47,7 +48,7 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
     },
     {
       type: "L",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance,
       y: Math.round(beltWidth * 2 * 10) / 20 + seamAllowance,
     },
   ];
@@ -55,12 +56,12 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
   const beltClosure: PathCommand[] = [
     {
       type: "M",
-      x: 2 * 3.14 * waistRadius + seamAllowance,
+      x: 2 * pi * waistRadius + seamAllowance,
       y: seamAllowance,
     },
     {
       type: "L",
-      x: 2 * 3.14 * waistRadius + seamAllowance,
+      x: 2 * pi * waistRadius + seamAllowance,
       y: seamAllowance + beltWidth * 2,
     },
   ];
@@ -68,128 +69,120 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
   const beltButton: PathCommand[] = [
     {
       type: "M",
-      x: seamAllowance + beltClosureAllowance / 2 + 5,
-      y: seamAllowance + beltWidth * 1.5 - 4,
+      x: seamAllowance + beltClosureAllowance / 2 + beltWidth * 0.35 + 0.5,
+      y: seamAllowance + beltWidth * 1.5,
     },
 
     {
       type: "A",
-      rx: 8,
-      ry: 8,
+      rx: beltWidth * 0.35,
+      ry: beltWidth * 0.35,
       rotation: 0,
       largeArc: 1,
       sweep: 0,
-      x: seamAllowance + beltClosureAllowance / 2 + 5,
-      y: seamAllowance + beltWidth * 1.5 + 4,
+      x: seamAllowance + beltClosureAllowance / 2 + beltWidth * 0.35 + 0.5,
+      y: seamAllowance + beltWidth * 1.5 + 0.5,
     },
 
     {
       type: "A",
-      rx: 8,
-      ry: 8,
+      rx: beltWidth * 0.35,
+      ry: beltWidth * 0.35,
       rotation: 0,
       largeArc: 0,
       sweep: 0,
-      x: seamAllowance + beltClosureAllowance / 2 + 5,
-      y: seamAllowance + beltWidth * 1.5 - 4,
+      x: seamAllowance + beltClosureAllowance / 2 + beltWidth * 0.35 + 0.5,
+      y: seamAllowance + beltWidth * 1.5,
     },
 
     // ===============
 
     {
       type: "M",
-      x: seamAllowance + beltClosureAllowance / 2 + 5 - 4,
-      y: seamAllowance + beltWidth * 1.5 - 1,
+      x: seamAllowance + beltClosureAllowance / 2 + beltWidth * 0.16,
+      y: seamAllowance + beltWidth * 1.5 - beltWidth * 0.04,
     },
 
     {
-      type: "A",
-      rx: 1,
-      ry: 1,
-      rotation: 0,
-      largeArc: 1,
-      sweep: 0,
-      x: seamAllowance + beltClosureAllowance / 2 + 5 - 4,
-      y: seamAllowance + beltWidth * 1.5 + 1,
+      type: "L",
+      x: seamAllowance + beltClosureAllowance / 2 + beltWidth * 0.16,
+      y: seamAllowance + beltWidth * 1.5 + beltWidth * 0.04,
     },
 
     {
-      type: "A",
-      rx: 1,
-      ry: 1,
-      rotation: 0,
-      largeArc: 0,
-      sweep: 0,
-      x: seamAllowance + beltClosureAllowance / 2 + 5 - 4,
-      y: seamAllowance + beltWidth * 1.5 - 1,
+      type: "M",
+      x: seamAllowance + beltClosureAllowance / 2 + beltWidth * 0.12,
+      y: seamAllowance + beltWidth * 1.5,
+    },
+
+    {
+      type: "L",
+      x: seamAllowance + beltClosureAllowance / 2 + beltWidth * 0.2,
+      y: seamAllowance + beltWidth * 1.5,
     },
 
     // ===============
 
     {
       type: "M",
-      x: seamAllowance + beltClosureAllowance / 2 + 5 - 10,
-      y: seamAllowance + beltWidth * 1.5 - 1,
+      x: seamAllowance + beltClosureAllowance / 2 - beltWidth * 0.12,
+      y: seamAllowance + beltWidth * 1.5 - beltWidth * 0.04,
     },
 
     {
-      type: "A",
-      rx: 1,
-      ry: 1,
-      rotation: 0,
-      largeArc: 1,
-      sweep: 0,
-      x: seamAllowance + beltClosureAllowance / 2 + 5 - 10,
-      y: seamAllowance + beltWidth * 1.5 + 1,
+      type: "L",
+      x: seamAllowance + beltClosureAllowance / 2 - beltWidth * 0.12,
+      y: seamAllowance + beltWidth * 1.5 + beltWidth * 0.04,
     },
 
     {
-      type: "A",
-      rx: 1,
-      ry: 1,
-      rotation: 0,
-      largeArc: 0,
-      sweep: 0,
-      x: seamAllowance + beltClosureAllowance / 2 + 5 - 10,
-      y: seamAllowance + beltWidth * 1.5 - 1,
+      type: "M",
+      x: seamAllowance + beltClosureAllowance / 2 - beltWidth * 0.08,
+      y: seamAllowance + beltWidth * 1.5,
+    },
+
+    {
+      type: "L",
+      x: seamAllowance + beltClosureAllowance / 2 - beltWidth * 0.16,
+      y: seamAllowance + beltWidth * 1.5,
     },
   ];
 
   const beltButtonHole: PathCommand[] = [
     {
       type: "M",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance / 2 - 2,
-      y: seamAllowance + beltWidth * 1.5 - 5,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance / 2 - 2,
+      y: seamAllowance + beltWidth * 1.5 - beltWidth * 0.3,
     },
 
     {
       type: "L",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance / 2 + 2,
-      y: seamAllowance + beltWidth * 1.5 - 5,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance / 2 + 2,
+      y: seamAllowance + beltWidth * 1.5 - beltWidth * 0.3,
     },
 
     {
       type: "M",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance / 2,
-      y: seamAllowance + beltWidth * 1.5 - 5,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance / 2,
+      y: seamAllowance + beltWidth * 1.5 - beltWidth * 0.3,
     },
 
     {
       type: "L",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance / 2,
-      y: seamAllowance + beltWidth * 1.5 + 5,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance / 2,
+      y: seamAllowance + beltWidth * 1.5 + beltWidth * 0.3,
     },
 
     {
       type: "M",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance / 2 - 2,
-      y: seamAllowance + beltWidth * 1.5 + 5,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance / 2 - 2,
+      y: seamAllowance + beltWidth * 1.5 + beltWidth * 0.3,
     },
 
     {
       type: "L",
-      x: 2 * 3.14 * waistRadius + seamAllowance + beltClosureAllowance / 2 + 2,
-      y: seamAllowance + beltWidth * 1.5 + 5,
+      x: 2 * pi * waistRadius + seamAllowance + beltClosureAllowance / 2 + 2,
+      y: seamAllowance + beltWidth * 1.5 + beltWidth * 0.3,
     },
   ];
 
@@ -198,13 +191,13 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
     {
       type: "L",
       // x: m.waist + beltClosureAllowance + seamAllowance * 2,
-      x: 2 * 3.14 * waistRadius + seamAllowance * 2 + beltClosureAllowance,
+      x: 2 * pi * waistRadius + seamAllowance * 2 + beltClosureAllowance,
       y: 0,
     }, // B
     {
       type: "L",
       // x: m.waist + beltClosureAllowance + seamAllowance * 2,
-      x: 2 * 3.14 * waistRadius + seamAllowance * 2 + beltClosureAllowance,
+      x: 2 * pi * waistRadius + seamAllowance * 2 + beltClosureAllowance,
       y: beltWidth * 2 + seamAllowance * 2,
     }, // C
     { type: "L", x: 0, y: beltWidth * 2 + seamAllowance * 2 }, // D
@@ -294,19 +287,19 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
 
     {
       type: "L",
-      x: seamAllowance + waistRadius + hipsRadius,
+      x: hipsRadius + seamAllowance,
       y: seamAllowance + 1,
     },
 
     {
       type: "L",
-      x: seamAllowance + waistRadius + hipsRadius,
+      x: hipsRadius + seamAllowance,
       y: seamAllowance + 11,
     },
 
     {
       type: "L",
-      x: seamAllowance + waistRadius + hipsRadius,
+      x: hipsRadius + seamAllowance,
       y: seamAllowance + 1,
     },
   ];
@@ -356,15 +349,15 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
         {
           kind: "text",
           x: seamAllowance + 10,
-          y: seamAllowance + 10,
+          y: seamAllowance + 8,
           text: "Юбка-солнце - пояс - 1 деталь",
           style: "title",
         },
 
         {
           kind: "text",
-          x: 3.14 * waistRadius,
-          y: Math.round(beltWidth * 2 * 10) / 20 + seamAllowance - 2,
+          x: pi * waistRadius,
+          y: Math.round(beltWidth * 2 * 10) / 20 + seamAllowance - 1,
           text: "Линия сгиба пояса",
           style: "label",
         },
@@ -402,15 +395,15 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
 
         {
           kind: "grainline",
-          x1: seamAllowance + 10,
+          x1: seamAllowance,
           y1: waistRadius + seamAllowance + 10,
-          x2: seamAllowance + 10,
+          x2: seamAllowance,
           y2: lengthRadius,
         },
 
         {
           kind: "text",
-          x: (seamAllowance + hipsRadius) / 2 + waistRadius - 8,
+          x: waistRadius + (hipsRadius - waistRadius) / 2,
           y: seamAllowance + 8,
           text: "Молния",
           style: "label",
@@ -451,7 +444,7 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
 
         {
           kind: "text",
-          x: seamAllowance + 12,
+          x: seamAllowance + 2,
           y: ((waistRadius + seamAllowance + lengthRadius) / 2) * 0.9,
           text: "Долевая нить",
           style: "label",
@@ -461,149 +454,3 @@ export function generateSkirtPattern(m: Measurements): PatternPart[] {
     },
   ];
 }
-
-// ==============================================================================================================
-
-// import type { Measurements, PathCommand } from "../../types/types";
-
-// export interface PatternPart {
-//   commands: PathCommand[];
-//   offsetX: number;
-//   offsetY: number;
-// }
-
-// export function generateSkirtPattern(m: Measurements): PatternPart[] {
-//   const seamAllowance = 10; // припуск на шов
-//   const waistAllowance = 15; // прибавка по талии на свободу облегания
-//   const hemAllowance = 15; // припуск на подгибку
-//   const beltClosureAllowance = 50; // прибавка к длине пояса на застежку
-//   const GAP = 10; // расстояние между деталями
-
-//   const beltWidth = m.beltWidth * 2; // двойная ширина пояса
-//   const waistRadius = Math.round(m.waist / 6.28) + waistAllowance; // радиус по талии
-//   const lengthRaduis = waistRadius + m.length; // радиус до подола юбки
-//   const r3 = Math.round(m.hips / 6.28); // радиус до линии бедер
-
-//   // ПОЯС — локально (0,0)
-//   const belt: PathCommand[] = [
-//     { type: "M", x: 0 + seamAllowance, y: 0 + seamAllowance }, // A
-//     {
-//       type: "L",
-//       x: m.waist + beltClosureAllowance + seamAllowance,
-//       y: 0 + seamAllowance,
-//     }, // B
-//     {
-//       type: "L",
-//       x: m.waist + beltClosureAllowance + seamAllowance,
-//       y: beltWidth + seamAllowance,
-//     }, // C
-//     { type: "L", x: 0 + seamAllowance, y: beltWidth + seamAllowance }, // D
-//     { type: "L", x: 0 + seamAllowance, y: 0 + seamAllowance },
-//     {
-//       type: "M",
-//       x: 0 + seamAllowance,
-//       y: Math.round(beltWidth * 10) / 20 + seamAllowance,
-//     },
-//     {
-//       type: "L",
-//       x: m.waist + beltClosureAllowance + seamAllowance,
-//       y: Math.round(beltWidth * 10) / 20 + seamAllowance,
-//     },
-//     { type: "Z" },
-
-//     { type: "M", x: 0, y: 0 }, // A
-//     {
-//       type: "L",
-//       x: m.waist + beltClosureAllowance + seamAllowance * 2,
-//       y: 0,
-//     }, // B
-//     {
-//       type: "L",
-//       x: m.waist + beltClosureAllowance + seamAllowance * 2,
-//       y: beltWidth + seamAllowance * 2,
-//     }, // C
-//     { type: "L", x: 0, y: beltWidth + seamAllowance * 2 }, // D
-//     { type: "L", x: 0, y: 0 },
-//     { type: "Z" },
-//   ];
-
-//   // ЮБКА — тоже локально (0,0)
-//   const skirt: PathCommand[] = [
-//     { type: "M", x: 0 + seamAllowance, y: waistRadius + seamAllowance }, // A
-//     {
-//       type: "A",
-//       rx: waistRadius,
-//       ry: waistRadius,
-//       rotation: 90,
-//       largeArc: 0,
-//       sweep: 0,
-//       x: waistRadius + seamAllowance,
-//       y: 0 + seamAllowance,
-//     }, // B
-//     { type: "L", x: lengthRaduis + seamAllowance, y: 0 + seamAllowance }, // C
-//     {
-//       type: "A",
-//       rx: lengthRaduis,
-//       ry: lengthRaduis,
-//       rotation: 90,
-//       largeArc: 0,
-//       sweep: 1,
-//       x: 0 + seamAllowance,
-//       y: lengthRaduis + seamAllowance,
-//     },
-//     { type: "Z" },
-
-//     // припуски
-
-//     { type: "M", x: 0, y: waistRadius },
-//     { type: "L", x: seamAllowance, y: waistRadius },
-//     {
-//       type: "A",
-//       rx: waistRadius - seamAllowance,
-//       ry: waistRadius - seamAllowance,
-//       rotation: 90,
-//       largeArc: 0,
-//       sweep: 0,
-//       x: waistRadius,
-//       y: seamAllowance,
-//     }, // B
-//     { type: "L", x: waistRadius, y: 0 },
-//     { type: "L", x: lengthRaduis + seamAllowance + hemAllowance, y: 0 }, // C
-//     {
-//       type: "L",
-//       x: lengthRaduis + seamAllowance + hemAllowance,
-//       y: seamAllowance,
-//     },
-//     {
-//       type: "A",
-//       rx: lengthRaduis + hemAllowance,
-//       ry: lengthRaduis + hemAllowance,
-//       rotation: 90,
-//       largeArc: 0,
-//       sweep: 1,
-//       x: seamAllowance,
-//       y: lengthRaduis + seamAllowance + hemAllowance,
-//     },
-//     {
-//       type: "L",
-//       x: 0,
-//       y: lengthRaduis + seamAllowance + hemAllowance,
-//     },
-//     { type: "Z" },
-//     { type: "M", x: r3, y: 0 },
-//     { type: "L", x: r3, y: 0 },
-//   ];
-
-//   return [
-//     {
-//       commands: belt,
-//       offsetX: 0,
-//       offsetY: 0,
-//     },
-//     {
-//       commands: skirt,
-//       offsetX: 0,
-//       offsetY: beltWidth + seamAllowance * 2 + GAP,
-//     },
-//   ];
-// }
